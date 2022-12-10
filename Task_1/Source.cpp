2175a20941ebc2e1e751461adec665c9ed56fe95
Alex Frol <drbo19777@gmail.com>
2022-12-10 14:54:00 +0300

    Init commit

---
+++ Task_1/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<windows.h>
+#include<string>
+
+int main(int argc, char** argv) {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string name;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+
+	system("pause");
+
+	return 0;
+}
