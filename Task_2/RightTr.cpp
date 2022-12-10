f76698185432ebeed2fe09dac5c304d8456929e1
Alex Frol <drbo19777@gmail.com>
2022-12-10 17:03:14 +0300

    Add Task_2

---
+++ Task_2/RightTr.cpp
@@ -0,0 +1,6 @@
+#include "RightTr.h"
+
+
+RightTr::RightTr(int a, int b, int c, int A, int B) : Triangle(a, b, c, A, B, 80) { 
+	Set_name("Прямоугольный треугольник: ");
+}
---
+++ Task_2/Square.cpp
@@ -0,0 +1,15 @@
+#include"Square.h"
+#include"EquilateralTr.h"
+#include"IsoscelesTr.h"
+#include"Parallelogram.h"
+#include"Quadrilateral.h"
+#include"Rectangle.h"
+#include"Rhomb.h"
+#include"RightTr.h"
+#include"Square.h"
+#include"Triangle.h"
+
+
+Square::Square(int a) : Rectangle(a, a) {
+	Set_name(" вадрат: ");
+}
